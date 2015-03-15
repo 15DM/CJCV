9634734ded315e356e7bef7fa85181c6ba611e8b
15DM <craigjones@iMac.local>
2015-03-15 21:12:53 +0000

    CV Microsite
    CJ CV Microsite

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>Craig Jones : Curriculum Vitae</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Fifteen Digital Media : Digital design, development and management">
+    <meta name="author" content=": Fifteen Digital Media">
+    
+    
+    
+    <link href="/a/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="/a/css/cv.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="/15assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/15assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/15assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/15assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="/15assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="/15assets/ico/favicon.png">
+
+    <link rel="icon" type="image/png" href="http://www.15dm.com/favicon.ico" />
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <header>
+
+        <div class="row">
+
+          <div class="col-xs-12">
+            <h1>Craig Jones</h1>
+          </div>
+
+          <div class="col-xs-6">
+            
+            <h5>UX/UI &amp; front end developer</h5>
+
+          </div>
+
+          <div class="col-xs-6 contact">
+            <h5><i class="fa fa-envelope"></i> <a href="mailto:craig.jones@15dm.com">craig.jones@15dm.com</a></h5>
+            <h5><i class="fa fa-phone"></i> +44 (0) 7968 121611</h5>
+          </div>
+
+        </div><!-- /row -->
+
+      </header>
+
+      <hr />
+
+      <section>
+
+        <div class="row item">
+
+          <div class="col-sm-12">
+
+            <h2>Profile</h2>
+
+            <p>Multimedia developer with 15 years experience, specialising in front end design and focusing on internet applications, mobile applications and offline training media. Experienced in HTML, CSS, Javascript, PHP, MySQL & database management. All web work completed to W3C standards.
+</p>
+            <p>Good experience of full stack development Experience of building bespoke CMS as well as working within standard frameworks. Great with time saving ideas &amp; delivery.</p>
+
+          </div>
+
+        </div><!-- /row -->
+
+      </section>
+
+
+      <section>
+
+        <div class="row item job">
+
+          <div class="col-sm-12">
+
+            <h2>Professional Experience</h2>
+
+            <p>Worked as a freelance designer/developer since 1999 & also fixed term contracts with agencies as part of a development team and also as a project manager.</p>
+
+            <p>Here is a brief summary of the larger projects that I have completed. Within this timeframe I have also completed many smaller projects.</p>
+
+            <h3>Job Title</h3>
+            <h4>Company Name</h4>
+            <h6>Job Dates</h6>
+            <ul class="jobdetails">
+              <li>Details</li>
+              <li>Details</li>
+              <li>Details</li>
+            </ul>
+            <ul class="skills">
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>JavaScript</li>
+              <li>Bootstrap</li>
+              <li>jQuery</li>
+              <li>Less</li>
+              <li>PHP</li>
+            </ul>
+          </div>
+
+        </div><!-- /row -->
+
+      </section>
+
+
+      <section>
+
+        <div class="row item">
+
+          <div class="col-sm-12">
+
+            <h2>Skills</h2>
+
+          </div>
+
+        </div><!-- /row -->
+
+      </section>
+
+      <section>
+
+        <div class="row item">
+
+          <div class="col-sm-12">
+
+            <h2>Education</h2>
+
+          </div>
+
+        </div><!-- /row -->
+
+      </section>
+
+
+      <section>
+
+        <div class="row item">
+
+          <div class="col-sm-12">
+
+            <h2>References</h2>
+
+            <p>References can be furnished upon request.</p>
+
+          </div>
+
+        </div><!-- /row -->
+
+      </section>
+
+    </div><!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/a/js/bootstrap.min.js"></script>
+
+  </body>
+  
+</html>
